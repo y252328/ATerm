1ee75394cfecaa394ab1de8334ca98165dc7cec2
Chih-Yu <y252328@gmail.com>
2019-12-26 23:27:15 +0800

    handle suddenly disconnect

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import time
 import serial
 import serial.tools.list_ports
+import serial.serialutil
 
 from PySide2.QtGui import QPixmap, QImage, QIcon
 from PySide2.QtWidgets import QApplication, QMainWindow, QFileDialog, QSizePolicy, QMenu, QMessageBox, QFileDialog
@@ -16,7 +17,6 @@ class AppWindow(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.inputLineEdit.returnPressed.connect(self.on_sendBtn_clicked)
         self.ser = None
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.read_from_ser)
@@ -29,9 +29,8 @@ class AppWindow(QMainWindow):
         event.accept()
 
     def read_from_ser(self):
-        if self.ser == None: return
-        if self.ser.in_waiting > 0:
-            text = self.ser.read(self.ser.in_waiting).decode('ascii')
+        if self.serial_in_waiting() > 0:
+            text = self.serial_read(self.serial_in_waiting()).decode('ascii')
             self.append_term(text)
 
     def append_term(self, text):
@@ -39,6 +38,38 @@ class AppWindow(QMainWindow):
         self.ui.outputTextBrowser.setText(text)
         scrollBar = self.ui.outputTextBrowser.verticalScrollBar()
         scrollBar.setValue(scrollBar.maximum())
+
+    def serial_in_waiting(self):
+        if self.ser != None:
+            try:
+                return self.ser.in_waiting
+            except serial.serialutil.SerialException:
+                self.on_connectBtn_clicked(True)
+        return 0
+
+    def serial_out_waiting(self):
+        if self.ser != None:
+            try:
+                return self.ser.out_waiting
+            except serial.serialutil.SerialException:
+                self.on_connectBtn_clicked(True)
+        return 0
+
+    def serial_read(self, n):
+        if self.ser != None:
+            try:
+                return self.ser.read(n)
+            except serial.serialutil.SerialException:
+                self.on_connectBtn_clicked(True)
+        return bytes()
+
+    def serial_write(self, data):
+        if self.ser != None:
+            try:
+                return self.ser.write(data)
+            except serial.serialutil.SerialException:
+                self.on_connectBtn_clicked(True)
+        return 0
 
     @Slot()
     def on_refreshBtn_clicked(self):
@@ -49,45 +80,46 @@ class AppWindow(QMainWindow):
         # self.ui.portComboBox.view().setMinimumWidth(100)
 
     @Slot()
-    def on_connectBtn_clicked(self):
-        if self.ui.connectBtn.text() == 'Connect':
+    def on_connectBtn_clicked(self, force_off=False):
+        if self.ui.connectBtn.text() == 'Connect' and not force_off:
             dev = self.ui.portComboBox.currentText().split('-')[0].strip()
             baud_rate = int(self.ui.baudComboBox.currentText())
             if dev == '': return
             self.ser = serial.Serial(dev, baud_rate)
             self.ui.connectBtn.setText('Disconnect')
             self.timer.start(200)
-            self.ui.sendBtn.setEnabled(False)
-            self.ui.sendFileBtn.setEnabled(False)
+            self.ui.inputLineEdit.returnPressed.connect(self.on_sendBtn_clicked)
+            self.ui.sendBtn.setEnabled(True)
+            self.ui.sendFileBtn.setEnabled(True)
         else:
             self.timer.stop()
             if self.ser != None:
                 self.ser.close()
+            self.ui.inputLineEdit.returnPressed.disconnect()
+            self.ui.sendBtn.setEnabled(False)
+            self.ui.sendFileBtn.setEnabled(False)
+            self.ser == None
             self.ui.connectBtn.setText('Connect')
-            self.ui.sendBtn.setEnabled(True)
-            self.ui.sendFileBtn.setEnabled(True)
 
     @Slot()
     def on_sendFileBtn_clicked(self):
-        if self.ser == None: return
         fileName = QFileDialog.getOpenFileName(parent=self, caption="Choose file", dir=os.getcwd())
         print(fileName[0])
         with open(fileName[0], 'rb') as f:
-            self.ser.write(f.read())
-        while self.ser.out_waiting > 0:
+            self.serial_write(f.read())
+        while self.serial_out_waiting() > 0:
             print('wait')
             time.sleep(0.1)
 
     @Slot()
     def on_sendBtn_clicked(self):
-        if self.ser == None: return
         eol = '\n'
         if self.ui.EOLComboBox.currentText() == 'CR;LF':
             eol = '\r\n'
         elif self.ui.EOLComboBox.currentText() == 'CR':
             eol = '\r'
         text = self.ui.inputLineEdit.text() + eol
-        n = self.ser.write(text.encode('ascii'))
+        self.serial_write(text.encode('ascii'))
         self.ui.inputLineEdit.setText('')
         self.append_term(text)
 
